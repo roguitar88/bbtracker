c42b77581a06810365e7cce4d59e0a08ee99b7b7
unknown <rogeriobsoares5@gmail.com>
2022-09-08 11:32:21 -0300

    trying to running node on https

--- server/index.js
+++ server/index.js
@@ -12,8 +12,9 @@ const User = require('./models/User');
 // const { QueryTypes } = require('sequelize');
 const hp = require('./helper');
 
-const privateKey  = fs.readFileSync('/etc/letsencrypt/live/bbtracker.tk/fullchain.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/bbtracker.tk/privkey.pem', 'utf8');
+const privateKey  = fs.readFileSync('/etc/letsencrypt/live/bbtracker.tk/fullchain.pem');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/bbtracker.tk/privkey.pem');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/bbtracker.tk/', 'utf8');
 
 const credentials = {key: privateKey, cert: certificate};
 // const bodyParser = require('body-parser');
