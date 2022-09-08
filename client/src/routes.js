ab557102fa3282f5567fef98dba19f4d9ba94a5e
unknown <rogeriobsoares5@gmail.com>
2022-09-08 20:08:04 -0300

    trying to running node on https

--- client/src/routes.js
+++ client/src/routes.js
@@ -13,7 +13,7 @@ function AppRoutes() {
     return(
         <Router>
             <Routes>
-                <Route path="/" element={!auth.isAuthenticated() === null ? <Main /> : <Navigate replace to="/welcome" />}></Route>
+                <Route path="/" element={!auth.isAuthenticated()? <Main /> : <Navigate replace to="/welcome" />}></Route>
                 <Route path="/signup" element={!auth.isAuthenticated() ? <Signup /> : <Navigate replace to="/welcome" />}></Route>
                 <Route path="/signin" element={!auth.isAuthenticated() ? <Signin /> : <Navigate replace to="/welcome" />}></Route>
                 <Route path="/welcome" element={!auth.isAuthenticated() ? <Navigate replace to="/signin" /> : <Welcome />}></Route>
