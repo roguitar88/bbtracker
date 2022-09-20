3443ae9adcb6b0afabb539a9012f8fcc0a086e4e
unknown <rogeriobsoares5@gmail.com>
2022-09-19 22:48:15 -0300

    Testing deployment

--- client/src/views/Signin/index.js
+++ client/src/views/Signin/index.js
@@ -41,7 +41,7 @@ const override: CSSProperties = {
 */
 
 function Signin() {
-  console.log("My plans will work on out");
+  console.log("What's going on?");
   /*
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# -- DEPLOYMENT STRATEGY --
-cd /var/www/html/bbtracker/client && /usr/bin/git pull && yarn build
